1d579ed2a42a57d0439264275939eacb82bbaf0e
Abdirahman Mohamed <mur7bac@gmail.com>
2022-10-11 17:03:21 +0300

    First Commit

---
+++ application/logs/log-2022-10-10.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2022-10-10 04:43:49 --> 404 Page Not Found: Public/images
+ERROR - 2022-10-10 05:10:29 --> Severity: Warning --> Trying to access array offset on value of type null /opt/lampp/htdocs/libby/application/views/view_contact.php 87
+ERROR - 2022-10-10 05:10:29 --> Severity: Warning --> Trying to access array offset on value of type null /opt/lampp/htdocs/libby/application/views/view_contact.php 87
+ERROR - 2022-10-10 05:10:29 --> Severity: Warning --> Trying to access array offset on value of type null /opt/lampp/htdocs/libby/application/views/view_contact.php 87
+ERROR - 2022-10-10 05:11:11 --> Severity: Warning --> fsockopen(): php_network_getaddresses: getaddrinfo for smtp_host failed: Name or service not known /opt/lampp/htdocs/libby/system/libraries/Email.php 2069
+ERROR - 2022-10-10 05:11:11 --> Severity: Warning --> fsockopen(): Unable to connect to ssl://smtp_host:465 (php_network_getaddresses: getaddrinfo for smtp_host failed: Name or service not known) /opt/lampp/htdocs/libby/system/libraries/Email.php 2069
+ERROR - 2022-10-10 05:18:35 --> Severity: Warning --> fsockopen(): php_network_getaddresses: getaddrinfo for smtp_host failed: Name or service not known /opt/lampp/htdocs/libby/system/libraries/Email.php 2069
+ERROR - 2022-10-10 05:18:35 --> Severity: Warning --> fsockopen(): Unable to connect to ssl://smtp_host:465 (php_network_getaddresses: getaddrinfo for smtp_host failed: Name or service not known) /opt/lampp/htdocs/libby/system/libraries/Email.php 2069
---
+++ application/logs/log-2022-10-11.php
@@ -0,0 +1,21 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2022-10-11 08:54:04 --> 404 Page Not Found: Public/js
+ERROR - 2022-10-11 08:54:04 --> 404 Page Not Found: Public/js
+ERROR - 2022-10-11 08:54:04 --> 404 Page Not Found: Public/css
+ERROR - 2022-10-11 08:54:08 --> 404 Page Not Found: Public/js
+ERROR - 2022-10-11 08:54:08 --> 404 Page Not Found: Public/js
+ERROR - 2022-10-11 08:54:08 --> 404 Page Not Found: Public/css
+ERROR - 2022-10-11 08:54:14 --> 404 Page Not Found: Public/css
+ERROR - 2022-10-11 08:54:14 --> 404 Page Not Found: Public/js
+ERROR - 2022-10-11 08:54:14 --> 404 Page Not Found: Public/js
+ERROR - 2022-10-11 08:54:24 --> 404 Page Not Found: Public/js
+ERROR - 2022-10-11 08:54:24 --> 404 Page Not Found: Public/js
+ERROR - 2022-10-11 08:54:24 --> 404 Page Not Found: Public/css
+ERROR - 2022-10-11 08:55:14 --> 404 Page Not Found: Public/js
+ERROR - 2022-10-11 08:55:14 --> 404 Page Not Found: Public/js
+ERROR - 2022-10-11 08:55:14 --> 404 Page Not Found: Public/css
+ERROR - 2022-10-11 08:55:16 --> 404 Page Not Found: Public/css
+ERROR - 2022-10-11 08:55:16 --> 404 Page Not Found: Public/js
+ERROR - 2022-10-11 08:55:19 --> 404 Page Not Found: Public/css
+ERROR - 2022-10-11 08:55:19 --> 404 Page Not Found: Public/js
